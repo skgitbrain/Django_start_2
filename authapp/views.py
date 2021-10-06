b95403e40822afefe5cc8e63348327a3b8bcbb64
skbrain <gitskbrain@bk.ru>
2021-10-07 01:35:42 +0300

    added lesson_2

--- authapp/views.py
+++ authapp/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 from django.core.mail import send_mail
 from django.conf import settings
 from authapp.models import ShopUser
+from django.db import transaction
+from authapp.forms import ShopUserProfileEditForm
+
 
 from authapp.forms import ShopUserEditForm
 
@@ -58,21 +61,32 @@ def register(request):
     content = {'title': title, 'register_form': register_form}
     
     return render(request, 'authapp/register.html', content)
-    
-    
+
+
+@transaction.atomic
 def edit(request):
     title = 'редактирование'
-    
+
     if request.method == 'POST':
-        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        edit_form = ShopUserEditForm(request.POST, request.FILES, \
+                                     instance=request.user)
+        profile_form = ShopUserProfileEditForm(request.POST, \
+                                               instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
-    
-    content = {'title': title, 'edit_form': edit_form}
-    
+        profile_form = ShopUserProfileEditForm(
+            instance=request.user.shopuserprofile
+        )
+
+    content = {
+        'title': title,
+        'edit_form': edit_form,
+        'profile_form': profile_form
+    }
+
     return render(request, 'authapp/edit.html', content)
 
 
